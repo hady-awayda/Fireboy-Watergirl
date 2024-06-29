bc8d7c5c1f075991ec6f5d501886aa63e68f17f3
Hady <hady_awayda@hotmail.com>
2024-06-29 18:52:58 +0300

    Fixed on reload position

--- Scripts/script.js
+++ Scripts/script.js
@@ -3,17 +3,14 @@ const parallax_el = document.querySelectorAll(".parallax");
 let xValue = window.innerWidth / 2,
   yValue = window.innerHeight / 2;
 
-window.addEventListener("mousemove", (event) => {
-  xValue = event.clientX - window.innerWidth / 2;
-  yValue = event.clientY - window.innerHeight / 2;
-
+const update = (cursorPosition) => {
   parallax_el.forEach((el) => {
     let speed = el.dataset.speed;
     let speedz = el.dataset.speedz;
     let isInLeft =
       parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
     let zValue =
-      (event.clientX - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
+      (cursorPosition - parseFloat(getComputedStyle(el).left)) * isInLeft * 0.1;
 
     el.style.transform = `translateX(calc(-50% + ${
       xValue * speed
@@ -21,4 +18,13 @@ window.addEventListener("mousemove", (event) => {
       yValue * speed * 0.66
     }px)) perspective(3300px) translateZ(${zValue * speedz}px)`;
   });
+};
+
+update(0);
+
+window.addEventListener("mousemove", (event) => {
+  xValue = event.clientX - window.innerWidth / 2;
+  yValue = event.clientY - window.innerHeight / 2;
+
+  update(event.clientX);
 });
