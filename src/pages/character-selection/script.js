948c9e3e8f5a76db4c052638d1eeebe73f6d1988
Hady <hady_awayda@hotmail.com>
2024-07-01 18:47:19 +0300

    trying a fix

--- src/pages/character-selection/script.js
+++ src/pages/character-selection/script.js
@@ -47,7 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
   renderElement("face");
   renderElement("fireboy");
   renderElement("watergirl");
-  renderElement("wall");
+  renderElement("floor");
 
   for (let i = 0; i < characters.length; i++) {
     renderElement(characters[i]);
