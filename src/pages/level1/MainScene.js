9a2acfd9d001902d5f177da34ddec39714b91235
Majd <majdbitar2003@gmail.com>
2024-07-03 02:49:14 +0300

    door-collision-finished

--- src/pages/level1/MainScene.js
+++ src/pages/level1/MainScene.js
@@ -9,8 +9,8 @@ class MainScene extends Phaser.Scene {
     this.p2y = p2Y;
     this.char1 = char1;
     this.char2 = char2;
-    this.characterTouchingGround = false;
-    this.canJump = true;
+    this.characterTouchingGround=false;
+    this.canJump=true;
   }
 
   preload() {
@@ -27,35 +27,15 @@ class MainScene extends Phaser.Scene {
 
     const jungleMap = this.make.tilemap({ key: "jungleMap" });
 
-    const jungleFloor = jungleMap.addTilesetImage(
-      "tiles-jungle",
-      "tiles-jungle-floor",
-      32,
-      32
-    );
-    const palm = jungleMap.addTilesetImage("palm", "tiles-palm", 32, 32);
-    const jungleBackGround = jungleMap.addTilesetImage(
-      "jungle-background",
-      "tiles-jungle-background",
-      32,
-      32
-    );
-    const door = jungleMap.addTilesetImage("door", "tiles-door", 32, 32);
+    const jungleFloor = jungleMap.addTilesetImage("tiles-jungle","tiles-jungle-floor",32,32);
+    const palm = jungleMap.addTilesetImage("palm", "tiles-palm",32,32);
+    const jungleBackGround = jungleMap.addTilesetImage("jungle-background","tiles-jungle-background",32,32);
+    const door = jungleMap.addTilesetImage("door", "tiles-door",32,32);
 
-    const jungleBackGroundLayer = jungleMap.createLayer(
-      "BG_Layer",
-      jungleBackGround,
-      -250,
-      0
-    );
-    const jungleFloorLayer = jungleMap.createLayer(
-      "Floor_Layer",
-      jungleFloor,
-      -250,
-      0
-    );
+    const jungleBackGroundLayer = jungleMap.createLayer("BG_Layer",jungleBackGround,-250,0);
+    const jungleFloorLayer = jungleMap.createLayer("Floor_Layer",jungleFloor,-250,0);
     const doorLayer = jungleMap.createLayer("Door_Layer", door, -250, 50);
-    const palmLayer = jungleMap.createLayer("Palm_Layer", palm, -250, 0);
+    const palmLayer = jungleMap.createLayer("Palm_Layer",palm,-250,0)
 
     jungleFloorLayer.setCollisionByProperty({ collision: true });
     doorLayer.setCollisionByProperty({ nextLevel: true });
@@ -79,7 +59,7 @@ class MainScene extends Phaser.Scene {
     });
 
     this.player1 = new Player({
-      label: "player1",
+      label : "player1",
       scene: this,
       x: this.p1x,
       y: this.p1y,
@@ -96,7 +76,7 @@ class MainScene extends Phaser.Scene {
       right: Phaser.Input.Keyboard.KeyCodes.D,
     });
     this.player2 = new Player({
-      label: "player2",
+      label : 'player2',
       scene: this,
       x: this.p2x,
       y: this.p2y,
@@ -108,14 +88,15 @@ class MainScene extends Phaser.Scene {
     this.add.existing(this.player2);
     this.player2.inputKeys = this.input.keyboard.createCursorKeys(); //detect arrow key presses for player2 (right-side)
 
+    
     this.player2.setFriction(0.05, 0.1, 0.01);
     this.player1.setFriction(0.05, 0.1, 0.01);
     this.player1.setScale(0.05);
-    this.player2.setScale(0.05);
-
+    this.player2.setScale(2)
+    
     // Detect collisions between players and the jungleFloorLayer
-    this.matter.world.on("collisionactive", (event) => {
-      console.log("checking collisionactive");
+    this.matter.world.on('collisionactive', (event) => {
+      console.log("checking collisionactive")
       event.pairs.forEach((pair) => {
         const { bodyA, bodyB } = pair;
 
@@ -123,31 +104,30 @@ class MainScene extends Phaser.Scene {
         console.log(bodyB.gameObject);
         //console.log(bodyA.label);
         //console.log(bodyB.label);
-
+        
         // Check if the collision involves the jungleFloorLayer and any of the players
         if (
-          (bodyA.label === "Circle Body" &&
-            bodyB.gameObject &&
-            bodyB.label === "Rectangle Body") ||
-          (bodyB.label === "Circle Body" &&
-            bodyA.gameObject &&
-            bodyA.label === "Rectangle Body")
-        ) {
-          this.characterTouchingGround = true;
-        }
+          (bodyA.label === 'Circle Body' && bodyB.gameObject && bodyB.label === "jungleFloorTile")
+          ||(bodyB.label === 'Circle Body' && bodyA.gameObject && bodyA.label === "jungleFloorTile")){
+            this.characterTouchingGround = true;
+            console.log("collided with floor");
+          }
+        if((bodyA.label === 'Circle Body' && bodyB.gameObject && bodyB.label === "doorTile")
+          ||(bodyB.label === 'Circle Body' && bodyA.gameObject && bodyA.label === "doorTile")){
+            this.characterTouchingGround = true;
+            console.log("collided with door");
+          }
       });
     });
-  }
 
+  }
+  
   update() {
+
     this.player1.update();
     this.player2.update();
-
-    if (
-      Phaser.Input.Keyboard.JustDown(this.player2.inputKeys.up) &&
-      this.characterTouchingGround &&
-      this.canJump
-    ) {
+    
+    if (Phaser.Input.Keyboard.JustDown(this.player2.inputKeys.up) && this.characterTouchingGround && this.canJump) {
       this.characterTouchingGround = false;
       this.canJump = false; // Disable further jumps
       // Set jump velocity
@@ -155,11 +135,7 @@ class MainScene extends Phaser.Scene {
     }
 
     // Check if player1 can jump
-    if (
-      Phaser.Input.Keyboard.JustDown(this.player1.inputKeys.up) &&
-      this.characterTouchingGround &&
-      this.canJump
-    ) {
+    if (Phaser.Input.Keyboard.JustDown(this.player1.inputKeys.up) && this.characterTouchingGround && this.canJump) {
       this.characterTouchingGround = false;
       this.canJump = false; // Disable further jumps
       // Set jump velocity
@@ -168,22 +144,20 @@ class MainScene extends Phaser.Scene {
 
     // Reset characterTouchingGround to false only when characters are in the air
     if (this.characterTouchingGround) {
-      if (
-        this.player1.body.velocity.y === 0 &&
-        this.player2.body.velocity.y === 0
-      ) {
+      if (this.player1.body.velocity.y === 0 && this.player2.body.velocity.y === 0) {
         this.characterTouchingGround = true;
       } else {
         this.characterTouchingGround = false;
       }
     }
 
-    if (!this.characterTouchingGround) {
+    if(!this.characterTouchingGround){
       setTimeout(() => {
         this.canJump = true; // Enable jumping again after timeout
       }, 300); // Adjust timeout duration as needed
     }
   }
-}
+  
+  }
 
 export default MainScene;
